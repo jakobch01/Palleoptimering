00023524d1925d0e48a974fd6551374f2369a1e9
jakobch01 <151140167+jakobch01@users.noreply.github.com>
2024-12-09 15:49:30 +0100

    Minor changes

--- Palleoptimering/obj/Debug/net8.0/Palleoptimering.AssemblyInfo.cs
+++ Palleoptimering/obj/Debug/net8.0/Palleoptimering.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Palleoptimering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7ab81c9b5bda382051bed7e0e7ad03f3de9b25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d9df745629e4ab3e59edf1e69ea09b27f86775c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Palleoptimering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Palleoptimering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
